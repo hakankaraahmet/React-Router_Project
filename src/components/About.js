1305cb8f4d5254d5a9d1496143b16abdc42451e7
hakankaraahmet <karaahmethkn@gmail.com>
2021-08-31 15:31:44 +0300

    modified about page

--- src/components/About.js
+++ src/components/About.js
@@ -1,15 +1,18 @@
-import React from 'react'
+import React from "react";
 
 const About = () => {
-    return (
-        <div 
-        className="ui rasied very padded text container segment"
-        style={{marginTop:"80px"}}
-        >
-            <h3 className="ui header">About</h3>
-            <p>Lorem ipsum dolor sit amet,consectetur adipiscing elit, sed do eiu amet</p>
-        </div>
-    )
-}
+  return (
+      <div
+        className="ui raised very padded text comtainer segment"
+        style={{ marginTop: "80px" }}
+      >
+        <h3 className="ui hader">About</h3>
+        <p>
+          Lorem ipsum dolar sit amet, concestetur adipiscing elit, sed do eiu
+          amet.
+        </p>
+      </div>
+  );
+};
 
-export default About
+export default About;
